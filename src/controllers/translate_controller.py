39ec069af7fc8274246b57ecb0d6211bb49186de
tulio <tulio.amorimm@gmail.com>
2023-08-14 17:48:08 -0300

    Feat(index): requisito 04

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+
 # from deep_translator import GoogleTranslator
-# from models.language_model import LanguageModel
+from models.language_model import LanguageModel
+
 # from models.history_model import HistoryModel
 
 
@@ -10,7 +12,13 @@ translate_controller = Blueprint("translate_controller", __name__)
 # Reqs. 4 e 5
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    # if request.method == "GET":
+    #     languages = LanguageModel.list_dicts()
+    # print(languages)
+    # return render_template("index.html", languages=languages)
+    languages = LanguageModel.list_dicts()
+    print(languages)
+    return render_template("index.html", languages=languages)
 
 
 # Req. 6
